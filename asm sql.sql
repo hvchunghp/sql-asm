ad8f1ecacc16b85ca9110f3b8259621cf29367af
hvchunghp <89526024+hvchunghp@users.noreply.github.com>
2022-08-30 10:14:20 +0700

    Update asm sql.sql

--- asm sql.sql
+++ asm sql.sql
@@ -15,7 +15,7 @@ create table Employee(
 	FOREIGN KEY (DepartID) REFERENCES Department(DepartId),
 	Salary Money,
 );
-
+--1
 insert into Department (DepartId, DepartName, Description)
 values (1, 'Marketing', 'In the era of technology 4.0, the role of the Marketing department is enhanced...'),
 (2, 'Human resource', 'Manage the organization''s human resources in a reasonable and effective manner...'),
@@ -25,23 +25,23 @@ insert into Employee (EmpCode, FirstName, LastName, Birthday, Gender, Address, D
 values ('MKT001', 'John', 'Doe', '1980-06-08', 1, 'USA', 1, 8000),
 ('HMR001', 'Marry', 'Rose', '1982-01-04', 0, 'USA', 2, 7000),
 ('MAN001', 'Carmine', 'Falcon', '1962-08-03', 1, 'USA', 3, 10000)
-
+--2
 UPDATE Employee 
 SET Salary = Salary + (Salary * 10/100);
-
+--3
 ALTER TABLE Employee
 ADD CHECK (Salary>0);
-
+--5
 create index IX_DepartmentName on Department(DepartName);
-
+--6
 CREATE VIEW emp_all as
 select Employee.EmpCode, Employee.FirstName, Employee.LastName, Department.DepartName  from Employee left join Department
 on Employee.DepartID = Department.DepartID
-
+--7
 create procedure sp_getAllEmp @departmentId int as
 select * from Employee where DepartId = @departmentId;
-
+--8
 create procedure sp_delDept @ID char(6) as 
 delete from Employee where EmpCode = @ID
 
-exec sp_delDept @ID = 'E1'
+exec sp_delDept @ID = 'MKT001'
